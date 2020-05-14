6c6cfa6a78062ba630d7a26037c2ebd29a907368
Herbert Knapp <herbert.knapp@wirecard.com>
2020-05-14 10:32:54 +0200

    Add IE svg-png swap and general browser fixes frame

---
+++ content/js/browser-fixes.js
@@ -0,0 +1,41 @@
+/**
+ * This is the palce to put all fixes required for wacky/outdated browsers
+ * like Safari, IE
+ * NOTE: This needs a JS transpiler to work in IE. Use e.g. Babel
+ */
+
+var isIE = /MSIE|Trident|Edge\//.test(window.navigator.userAgent);
+var isSafariDesktop = (/Safari/i.test(uA) && /Apple Computer/.test(vendor))
+var isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
+    (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
+    !window.MSStream
+
+function swapSVGandPNG() {
+    document.querySelectorAll('img').forEach((img) => {
+        requestIdleCallback(() => {
+            img.src = img.src.replace(new RegExp('\.svg$'), '.png');
+        });
+    });
+}
+
+/**
+ * Load fixes at DOMContentLoaded and call this again on every pageSwitch
+ */
+function loadBrowserFixes() {
+    switch (true) {
+        case isIE:
+            swapSVGandPNG();
+            break;
+
+        case isSafariDesktop:
+            break;
+
+        case isIOS:
+            break;
+
+        default:
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', loadBrowserFixes);
